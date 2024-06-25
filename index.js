7b16abaaae2831c6c10b273bccec6cefd5b92a1c
WAHU0508 <sharongikenye@gmail.com>
2024-06-25 19:25:43 +0300

    Initialize variables using const and let

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+//test-1 companyName set as "Scuber" and defined as a const
+const companyName = "Scuber";
+
+//test-2 mostProfitableNeighborhood set as "Chelsea" and defined using let
+let mostProfitableNeighborhood = "Chelsea";
+
+//test-3  companyCeo set as "Susan Smith" and defined using let
+let companyCeo = "Susan Smith"
